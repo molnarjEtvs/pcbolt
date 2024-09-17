eb6893be7fcc8fdbc332ba4aef97ee72c42cabc8
Dobronyi Csaba <dobronyicsaba@elg-bp.edu.hu>
2024-09-17 18:32:21 +0200

    Csaba kész

---
+++ resources/views/dcsaba.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+@section('title','Csaba')
+@section('content')
+  <h1>Szia Csaba</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\jozsefController;
+use App\Http\Controllers\dcsabaController;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('fooldal');
 
 Route::get('/jozsef',[jozsefController::class,"index"])->name('menu1');
+
+Route::get('/dcsaba',[dcsabaController::class,"index"])->name('menu_dcsaba');
